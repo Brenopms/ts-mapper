26d86c3ff137a702c8bdbc162f82a84d402a04e4
Brenopms <brenosilva1@gmail.com>
2022-08-01 12:31:14 -0300

    test: add coverage reports to test config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['html', 'json']
+    }
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
   },
