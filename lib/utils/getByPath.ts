ee2b6c23188c5ed929628ba499f00d499f38aaa5
Brenopms <brenosilva1@gmail.com>
2022-07-31 21:38:26 -0300

    refactor: update getByPath file name

---
+++ lib/utils/getByPath.ts
@@ -0,0 +1,19 @@
+import { GenericObject } from "../interfaces/genericObj";
+import { Paths } from "../interfaces/paths.interface";
+
+export function getByPath<T extends GenericObject, O = unknown, S = unknown>(
+  obj: T,
+  path: Paths<T, 3> & string,
+  defaultValue?: S
+): O;
+
+export function getByPath<T extends GenericObject, O = unknown>(
+  obj: T,
+  path: Paths<T, 3> & string,
+  defaultValue?: unknown
+): O {
+  const traversablePath = path?.split(".");
+  return traversablePath?.reduce((acc, curr) => {
+    return acc ? acc[curr] : defaultValue;
+  }, obj) as any;
+}
