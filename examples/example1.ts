0cf6f28b1a3bf0c801eceb8d8166801de0750df2
Brenopms <brenosilva1@gmail.com>
2022-08-01 14:56:32 -0300

    fix: update examples to new signatures

--- examples/example1.ts
+++ examples/example1.ts
@@ -1,45 +1,44 @@
-import { mapper } from '../lib/mapper'
-import { Mapper } from '../lib/interfaces/mapper'
-import { getByPath } from '../lib/utils/pathGet'
+import { mapper } from "../lib/mapper/mapper";
+import { Mapper } from "../lib/interfaces/mapper.interface";
+import { getByPath, Getter } from "../lib/utils/getByPath";
 
 interface Input1 {
-  prop1: number,
-  prop2: number,
-  prop3: string
+  prop1: number;
+  prop2: number;
+  prop3: string;
 }
 
 interface Output1 {
-  prop10: number,
-  prop20: number,
-  prop30: string
+  prop10: number;
+  prop20: number;
+  prop30: string;
 }
 
 const input: Input1 = {
   prop1: 100,
   prop2: 200,
-  prop3: 'string'
-}
+  prop3: "string",
+};
 
 const mapper2: Mapper<Input1, Output1> = [
   {
-    srcPath: 'prop1',
-    dstPath: 'prop10',
-    defaultValue: 'test'
+    srcPath: "prop1",
+    dstPath: "prop10",
+    defaultValue: "test",
   },
   {
-    srcPath: 'prop2',
-    dstPath: 'prop20',
-    defaultValue: 'test2',
+    srcPath: "prop2",
+    dstPath: "prop20",
+    defaultValue: "test2",
   },
   {
-    dstPath: 'prop30',
-    defaultValue: 'test3',
-    transform(input: Input1) {
-      return getByPath<Input1>(input, 'prop2') + 'AppendTest'
-    }
-  }
-]
+    dstPath: "prop30",
+    defaultValue: "test3",
+    transform(getter) {
+      return getter('prop1', 'string') + "AppendTest";
+    },
+  },
+];
 
-const output = mapper<Input1, Output1>(input, mapper2)
+const output = mapper<Input1, Output1>(input, mapper2);
 console.log(output);
-
